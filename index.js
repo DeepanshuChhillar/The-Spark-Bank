fc35f8ab6371f41a1233c62d8af94c027e5bd806
DeepanshuChhillar <deepanshu29012001@gmail.com>
2024-04-14 10:59:32 +0530

    User Account Creation Module

--- index.js
+++ index.js
@@ -7,6 +7,9 @@ const port=3000;
 const dir_name=dirname(fileURLToPath(import.meta.url));
 const app=express();
 
+var mainDb=[];
+var idCount=0;
+
 app.use(bodyParser.urlencoded({extended:false}));
 app.use(express.static("public"));
 
@@ -19,6 +22,27 @@ app.get('/', (req,res) => {
     res.locals={
         name: "Deep",
     };
-    res.render(res.render(dir_name+"/public/index.ejs"));
+    res.render(dir_name+"/public/index.ejs");
 });
+
+app.post("/createUser", (req,res) => 
+{
+    idCount++;
+    var obj={
+        userName: req.body["Name"],
+        balanceAmount: req.body["Amount"],
+        userId: idCount
+    }
+    mainDb.push(obj);
+    console.log(mainDb);
+    res.redirect("/userDetails");
+});
+
+app.get("/userDetails", (req,res) => {
+    res.locals={
+        myArr: mainDb
+    };
+    res.render(dir_name+"/public/accDetails.ejs");
+});
+
 
